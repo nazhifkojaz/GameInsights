dcbaafac2aecc5410a906eec535d571d7b174bd9
nazhifkojaz <nazhifkojaz@gmail.com>
2026-04-07 02:18:07 +0700

    refactor: remove GameSearch utility and rapidfuzz dependency

--- gameinsights/utils/__init__.py
+++ gameinsights/utils/__init__.py
@@ -1,6 +1,5 @@
-from .gamesearch import GameSearch
 from .import_optional import import_pandas
 from .logger import LoggerWrapper
 from .metrics import MetricsCollector, metrics
 
-__all__ = ["GameSearch", "LoggerWrapper", "MetricsCollector", "metrics", "import_pandas"]
+__all__ = ["LoggerWrapper", "MetricsCollector", "metrics", "import_pandas"]
